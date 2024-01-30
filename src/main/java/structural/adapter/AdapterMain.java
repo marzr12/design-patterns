7e60451a754cbaac53fd9a3abcbf0570f99d0abe
marzr12 <marzr12@gmail.com>
2024-01-29 20:56:29 -0500

    Adding additional class files

---
+++ src/main/java/structural/adapter/AdapterMain.java
@@ -0,0 +1,9 @@
+package structural.adapter;
+
+public class AdapterMain {
+
+    public static void main(String [] args){
+
+    }
+
+}
---
+++ src/main/java/structural/bridge/BridgeMain.java
@@ -0,0 +1,9 @@
+package structural.bridge;
+
+public class BridgeMain {
+
+    public static void main(String [] args){
+
+    }
+
+}
